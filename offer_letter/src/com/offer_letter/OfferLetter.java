37d7b5584e669c53a705e80c2642ea4cccca6876
akshay-surjuse <akshay1surjuse@gmail.com>
2024-01-31 19:16:26 +0530

    offer_letter commited

---
+++ offer_letter/src/com/offer_letter/OfferLetter.java
@@ -0,0 +1,10 @@
+package com.offer_letter;
+
+public class OfferLetter 
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Xyz send you an offer letter for Java Developer...");
+
+	}
+}
