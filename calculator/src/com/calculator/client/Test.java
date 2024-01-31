65290c0e5897df0a7401bd756c670ddd0e3d8b59
akshay-surjuse <akshay1surjuse@gmail.com>
2024-01-31 13:03:29 +0530

    calculator project commited

---
+++ calculator/src/com/calculator/client/Test.java
@@ -0,0 +1,11 @@
+package com.calculator.client;
+
+import com.calculator.model.Calculator;
+
+public class Test
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(Calculator.m1(10, 20));
+	}
+}
---
+++ calculator/src/com/calculator/model/Calculator.java
@@ -0,0 +1,9 @@
+package com.calculator.model;
+
+public class Calculator 
+{
+	public static Integer m1(int x, int y)
+	{
+		return x+y;
+	}
+}
